9a0fcab8543709fae2abc4415e12635f3aee5fbc
Gunnar Hjalmarsson <gunnarhj@ubuntu.com>
2017-05-06 21:59:00 +0200

    17.10.4 (patches unapplied)
    Imported using git-ubuntu import.

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,8 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="ubuntu-docs"
 
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/gnome-help) || {
+(test -f $srcdir/configure.ac \
+  && test -d $srcdir/ubuntu-help) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
--- scripts/schematron.sh
+++ scripts/schematron.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "${PWD##*/}" != 'scripts' -o ! -d ../gnome-help ]; then
+if [ "${PWD##*/}" != 'scripts' -o ! -d ../ubuntu-help ]; then
     echo "ERROR: You should run this script from the 'scripts' directory."
     exit 1
 fi
-cd ../gnome-help/C
+cd ../ubuntu-help/C
 
 xmllint --noout --schematron gnome-help.sct *.page 2>&1 | grep -v validates
